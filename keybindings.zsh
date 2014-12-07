e82c7b961eefc429cd1d8b5a8828364c3e14249f
Rauli Laine <rauli.laine@gmail.com>
2014-12-07 15:51:45 +0200

    Add some meta keybindings which work on FreeBSD.

--- keybindings.zsh
+++ keybindings.zsh
@@ -1,1 +1,10 @@
+bindkey " " magic-space
 bindkey "^i" _my_tab_completion
+
+bindkey "^[[1~" beginning-of-line
+bindkey "^[[4~" end-of-line
+bindkey "^[[1;5D" backward-word
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5A" up-line-or-history
+bindkey "^[[1;5B" down-line-or-history
+bindkey "^[[3~" delete-char
