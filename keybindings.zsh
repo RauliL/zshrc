4e44fa6ddf096813d7b34e2a211afaf5f3d75fe4
Rauli Laine <rauli.laine@gmail.com>
2014-12-26 15:19:09 +0200

    Define more keybindings.

--- keybindings.zsh
+++ keybindings.zsh
@@ -1,6 +1,26 @@
 bindkey " " magic-space
+bindkey "^a" beginning-of-line
+bindkey "^b" backward-char
+bindkey "^d" delete-char-or-list
+bindkey "^e" end-of-line
+bindkey "^f" forward-line
+bindkey "^g" send-break
+bindkey "^h" backward-delete-char
 bindkey "^i" _my_tab_completion
+bindkey "^j" accept-line
+bindkey "^k" kill-line
+bindkey "^l" clear-screen
+bindkey "^m" accept-line
+bindkey "^n" down-line-or-history
+bindkey "^o" accept-line-and-down-history
+bindkey "^p" up-line-or-history
+bindkey "^q" push-line
 bindkey "^r" history-incremental-search-backward
+bindkey "^s" history-incremental-search-forward
+bindkey "^t" transpose-chars
+bindkey "^u" kill-whole-line
+bindkey "^v" quoted-insert
+bindkey "^w" backward-kill-word
 
 case "$TERM" in
     linux)
