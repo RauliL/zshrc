3de24cf3243703b56974e071d5f557c6eae93091
Rauli Laine <rauli.laine@anders.fi>
2016-08-16 16:00:27 +0300

    Tweak `wo` alias a bit.

--- functions.zsh
+++ functions.zsh
@@ -3,11 +3,19 @@ wo()
     local -a project
     project=$1
 
-    if [[ -d "$HOME/Projektit/$project/project" ]]
-    then
-        source "$HOME/Projektit/$project/venv/bin/activate"
-        cd "$HOME/Projektit/$project/project"
-    else
-        echo "Unrecognized project: $project"
-    fi
+    for app_dir in app project
+    do
+        project_dir="$HOME/Projektit/$project"
+        if [[ -d "$project_dir/$app_dir" && -d "$project_dir/venv" ]]
+        then
+            if [[ -n "$VIRTUAL_ENV" ]]
+            then
+                deactivate
+            fi
+            source "$project_dir/venv/bin/activate"
+            cd "$project_dir/$app_dir"
+            return
+        fi
+    done
+    echo "Unrecognized project: $project"
 }
